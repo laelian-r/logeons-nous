3aa838761de8c3814ab38df96369f249a735bb2c
lroux07 <lroux@edenschool.fr>
2025-02-08 22:55:35 +0100

    Implement apartment listing with FlatList and update location city in data

--- app/screens/home.js
+++ app/screens/home.js
@@ -1,5 +1,6 @@
-import { StyleSheet, View, Text } from 'react-native';
+import { StyleSheet, View, FlatList, Text, ImageBackground } from 'react-native';
 import AntDesign from '@expo/vector-icons/AntDesign';
+import houseList from '@/assets/data/data-apartments.json';
 
 const home = () => {
     return (
@@ -9,19 +10,58 @@ const home = () => {
                 <AntDesign name="search1" size={30} />
             </View>
 
-            <View style={styles.section}>
-                <View style={styles.article}>
-                    <Text>Article 1</Text>
+            <FlatList 
+             data={houseList} 
+             horizontal 
+             contentContainerStyle={{gap: 20}} 
+            //  keyExtractor={
+            //     (item) => item.cover_image_url.toLowerCase().toString() + 
+            //     Math.floor(Math.random() * 1000)
+            //  } 
+             renderItem={({item}) => 
+                <View style={styles.articleContainer}>
+                    <ImageBackground 
+                     source={{
+                        uri: item.cover_image_url
+                     }} 
+                     style={styles.articleImage}
+                    >
+
+                        <View style={styles.tagContainer}>
+                            <View style={styles.tag}>
+                                <Text style={styles.tagText}>Nouveau</Text>
+                            </View>
+                        </View>
+
+                    </ImageBackground>
+
+                    <View style={styles.infosLocation}>
+                        <Text style={styles.location}>{item.location_city}</Text>
+                        <View style={styles.articleLabels}>
+                            <AntDesign name="star" size={24} color="#fe9379" />
+                            <Text>5.0</Text>
+                        </View>
+                    </View>
+
+                    <View style={styles.infosLocation}>
+                        <Text style={styles.price}>{item.month_price}€ / mois</Text>
+                        <View style={styles.articleLabels}>
+                            <AntDesign name="user" size={24} color="#fe9379" />
+                            <Text>3 visiteurs</Text>
+                        </View>
+                    </View>
                 </View>
-            </View>
+            } />
         </View>
     );
 };
 
 const styles = StyleSheet.create({
-    // body: {
-    //     padding: '10%',
-    // },
+    body: {
+        marginHorizontal: 15,
+        display: 'flex',
+        gap: 20,
+    },
 
     header: {
         display: 'flex',
@@ -33,6 +73,63 @@ const styles = StyleSheet.create({
 
     headerText: {
         fontSize: 30,
+    },
+
+    articleContainer: {
+        display: 'flex',
+        gap: 10,
+    },
+
+    articleImage: {
+        height: 200,
+        width: 300,
+        borderRadius: 30,
+        overflow: 'hidden',
+        padding: 15,
+    },
+
+    tagContainer: {
+        display: 'flex',
+        flexDirection: 'row',
+    },
+
+    tag: {
+        backgroundColor: '#4e5ac8',
+        paddingHorizontal: 10,
+        paddingVertical: 5,
+        borderRadius: 50,
+        opacity: 0.8,
+    },
+
+    tagText: {
+        color: 'white',
+    },
+
+    infosLocation: {
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+    },
+
+    location: {
+        fontSize: 25,
+    },
+
+    price: {
+        fontSize: 20,
+        color: '#8a8a8a',
+    },
+
+    articleLabels: {
+        display: 'flex',
+        flexDirection: 'row',
+        gap: 10,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: 'rgba(243,243,243,255)',
+        padding: 10,
+        borderRadius: 5,
     },
 });
 
