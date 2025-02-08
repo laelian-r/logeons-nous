b12568efc298432a1b63e341a10da1cca73749a7
lroux07 <lroux@edenschool.fr>
2025-02-08 19:24:50 +0100

    Add search icon to home screen

--- app/screens/home.js
+++ app/screens/home.js
@@ -1,9 +1,11 @@
 import {View, Text} from 'react-native';
+import AntDesign from '@expo/vector-icons/AntDesign';
 
 const home = () => {
     return (
-        <View>
+        <View style>
             <Text>Découvrir</Text>
+            <AntDesign name="search1" size={24} color="black" />
         </View>
     );
 };
