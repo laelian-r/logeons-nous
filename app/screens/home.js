c6576c5d0e6a2f794a0cba2e0636636027591690
lroux07 <lroux@edenschool.fr>
2025-02-08 20:21:34 +0100

    Sample apartment data

--- app/screens/home.js
+++ app/screens/home.js
@@ -1,13 +1,39 @@
-import {View, Text} from 'react-native';
+import { StyleSheet, View, Text } from 'react-native';
 import AntDesign from '@expo/vector-icons/AntDesign';
 
 const home = () => {
     return (
-        <View style>
-            <Text>Découvrir</Text>
-            <AntDesign name="search1" size={24} color="black" />
+        <View style={styles.body}>
+            <View style={styles.header}>
+                <Text style={styles.headerText}>Découvrir</Text>
+                <AntDesign name="search1" size={30} />
+            </View>
+
+            <View style={styles.section}>
+                <View style={styles.article}>
+                    <Text>Article 1</Text>
+                </View>
+            </View>
         </View>
     );
 };
+
+const styles = StyleSheet.create({
+    // body: {
+    //     padding: '10%',
+    // },
+
+    header: {
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center', 
+        marginTop: 50,
+    },
+
+    headerText: {
+        fontSize: 30,
+    },
+});
 
 export default home;
