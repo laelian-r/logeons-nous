2a89f6fdc0ba0af1e50809446cb05f2aaa08ad70
lroux07 <lroux@edenschool.fr>
2025-02-08 18:51:13 +0100

    Setup project

---
+++ app/index.js
@@ -0,0 +1,11 @@
+import { View, Text } from "react-native";
+
+const page = () => {
+    return (
+        <View>
+            <Text>Ceci est un texte</Text>
+        </View>
+    );
+};
+
+export default page;
